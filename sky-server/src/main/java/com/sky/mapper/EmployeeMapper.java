8dc641174e27fefd11607fec1c4728181efb2d11
Yaostar <987165181@qq.com>
2024-10-10 20:26:31 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -23,4 +23,6 @@ public interface EmployeeMapper {
     void insert(Employee employee);
     //动态sql
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+    //根据主键动态修改属性
+    void update(Employee employee);
 }
